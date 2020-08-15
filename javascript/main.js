15490770c5b3b2804755e87b0da18a7c494cf475
Donald-Brown <donbrown4652@gmail.com>
2020-08-15 10:15:16 -0500

    moves main.js

--- javascript/main.js
+++
@@ -1,8 +0,0 @@
-import { convertCtoF, convertFtoC, convertKgToLbs, convertLbsToKg } from './convertJS.js';
-import logText from './test.js';
-
-console.log(convertCtoF(0));
-console.log(convertFtoC(32));
-console.log(convertLbsToKg(99.208));
-console.log(convertKgToLbs(45));
-logText(0);
